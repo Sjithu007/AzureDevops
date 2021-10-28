fb90013e7b01c5b8b64062411aedf23bc6407d23
Sathyajith K P <sathyajithkp10@gmail.com>
2021-10-28 09:44:22 +0530

    First Commit

---
+++ test_app.py
@@ -0,0 +1,15 @@
+from app import app
+import pytest
+
+def test_app():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
+    assert response.data == b'Hello World'
+    
+def test_index():   
+    response = app.test_client().get('/index')
+    assert response.status_code == 200
+    assert b'Welcome Sathyajith' in response.data
+    assert b'Click Here' in response.data
+    
+    
